1cb90477bf7be78e5f8e6ed8402227d0543f6452
kykomiyazakih1993 <47366030+kykomiyazakih1993@users.noreply.github.com>
2019-02-05 20:22:27 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,140 @@
+
+const Discord = require("discord.js");
+
+
+const used = new Set();
+
+
+const client = new Discord.Client();
+
+
+const config = require("./config.json");
+
+
+client.on("ready", () => {
+
+  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+
+  client.user.setActivity(`Something u can't afford`);
+});
+
+client.on("guildCreate", guild => {
+
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  client.user.setActivity(`Something u can't afford`);
+});
+
+client.on("guildDelete", guild => {
+
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  client.user.setActivity(`Something u can't afford`);
+});
+
+
+client.on("message", async message => {
+
+  if(message.author.bot) return;
+  
+
+  if(message.content.indexOf(config.prefix) !== 0) return;
+  
+
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  
+
+  
+  if(command === "ping") {
+
+    const m = await message.channel.send("Ping?");
+    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+  }
+
+  if(command === "spotify") {
+   
+    
+    client.channels.get("542356943172403211").fetchMessages({limit: 20}).then(m => {
+        let arr = m.array()
+        let y = arr[0]
+        
+        if(used.has(message.author.id)) return message.channel.send("You can only get 2 accounts in 1 day!")
+        else {
+        if(!y) return message.reply("Sorry, POLAT didn't add the spotify accounts to the list yet, it will be coming soon!")
+        message.author.send(y.content).then(y.delete())
+        }
+
+        used.add(message.author.id);
+      setTimeout(() => {
+        used.delete(message.author.id);
+      }, 1000 * 86400)
+        
+    })
+}
+
+  if(command === "say") {
+  
+    const sayMessage = args.join(" ");
+
+    message.delete().catch(O_o=>{}); 
+
+    message.channel.send(sayMessage);
+  }
+  
+  if(command === "kick") {
+  
+    if(!message.member.roles.some(r=>["Owner", "CO Owner", "Head Moderator", "Moderator"].includes(r.name)) )
+      return message.reply("Sorry, you don't have permissions to use this!");
+    
+
+    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+    if(!member)
+      return message.reply("Please mention a valid member of this server");
+    if(!member.kickable) 
+      return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
+    
+  
+    let reason = args.slice(1).join(' ');
+    if(!reason) reason = "No reason provided";
+    
+  
+    await member.kick(reason)
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
+    message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
+
+  }
+  
+  if(command === "ban") {
+    
+    if(!message.member.roles.some(r=>["Owner", "CO Owner", "Head Moderator"].includes(r.name)) )
+      return message.reply("Sorry, you don't have permissions to use this!");
+    
+    let member = message.mentions.members.first();
+    if(!member)
+      return message.reply("Please mention a valid member of this server");
+    if(!member.bannable) 
+      return message.reply("I cannot ban this user! Do they have a higher role? Do I have ban permissions?");
+
+    let reason = args.slice(1).join(' ');
+    if(!reason) reason = "No reason provided";
+    
+    await member.ban(reason)
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't ban because of : ${error}`));
+    message.reply(`${member.user.tag} has been banned by ${message.author.tag} because: ${reason}`);
+  }
+  
+  if(command === "purge") {
+    
+    const deleteCount = parseInt(args[0], 10);
+    
+    
+    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
+      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
+    
+    
+    const fetched = await message.channel.fetchMessages({limit: deleteCount});
+    message.channel.bulkDelete(fetched)
+      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
